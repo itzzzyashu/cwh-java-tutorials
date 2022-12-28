e0fc648d270d80a7b034e1ba47f491a2e1f0c428
Itzzzyashu <sakshamvirmani86@gmail.com>
2022-12-28 19:41:54 +0530

    Arrays

--- Chapters/Arrays.java
+++ Chapters/Arrays.java
@@ -1,5 +1,15 @@
 public class Arrays {
-    public static void main(String[] args) {
-        int [] marks = new int[5];
-    }
+  public static void main(String[] args) {
+    int[] marks = new int[5];
+    marks[0] = 100;
+    marks[1] = 60;
+    marks[2] = 70;
+    marks[3] = 90;
+    marks[4] = 80;
+    // System.out.println(marks[4]);
+
+    int[] marks2 = {100, 70, 80, 90, 50};
+    // System.out.println(marks2[4]);
+    System.out.println(marks2.length);
+  }
 }
