c1d09628d90549c9c623e129001b8c5a4848dfe6
Itzzzyashu <sakshamvirmani86@gmail.com>
2022-12-28 19:17:07 +0530

    by While Loop

--- Exercices/MultiplyTables.java
+++ Exercices/MultiplyTables.java
@@ -2,12 +2,26 @@
 
 public class MultiplyTables {
   public static void main(String[] args) {
+    // 2 For Loop
     int num_of_tables = 20;
     for (int i = 1; i <= num_of_tables; i++) {
       System.out.println("\nThe table of " + i + " is");
       for (int n = 1; n <= 10; n++) {
         System.out.println(i + " x " + n + " = " + i * n);
       }
+    }
+
+    // 12 While Loop
+    int num_of_tables = 20;
+    int i = 1;
+    while (i <= num_of_tables) {
+      System.out.println("\nThe table of " + i + " is");
+      int n = 1;
+      while (n <= 10) {
+        System.out.println(i + " x " + n + " = " + i * n);
+        n++;
+      }
+      i++;
     }
   }
 }
