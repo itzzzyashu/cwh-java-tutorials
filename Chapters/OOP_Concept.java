2366331fc8b6ccebacc3c516fab88638a2c902b9
Itzzzyashu <sakshamvirmani86@gmail.com>
2022-12-29 16:12:27 +0530

    OOP Concent and Terminologies/Definitions

---
+++ Chapters/OOP_Concept.java
@@ -0,0 +1,34 @@
+/*
+
+Object-oriented programming aims to implement real-world entities
+like inheritance, hiding, polymorphism etc. in programming.
+
+DRY stands for Don't Repeat Yourself.
+In Java, it means don't write the same code repeatedly.
+Suppose you are having the same code at many places in your program,
+then it means you are not following the DRY approach;
+You are repeating the same code at different places.
+
+Object Oriented (OO) Terminology/Definitions
+Class: Pattern or blueprint for creating an object. A class contains all attributes and behaviors that describe or make up the object.
+Object: A thing that can be seen and touched (and manipulated). It is an instance of a class.
+In a bit more detail, a (software) object is a construct that combines a state (data) and behavior (operations). When combined, the state and the behavior represent an abstraction of a "real-world" object. See example here.
+
+Model the problem in OOPs
+-----------------------------------------------------
+| Noun       | Class      | Employee                 |    
+| Adjective  | Attributes | name, age, salary        |
+| Verb       | Methods    | getSalary(), increment() |
+------------------------------------------------------
+
+OOP Terminology/Definitions
+▶ Attributes: Characteristics that describe the object (sometimes referred to as properties).
+▶ Methods: Operations (or actions) that objects perform or operations which are performed to an object. Sometimes referred to as behaviors.
+▶ Instance: An object created from a class (is an instance of that class).
+▶ Abstraction: Refers to hiding the internal details of an object from the user.
+▶ Encapsulation: Refers to the combining of an object's attributes and behaviors into a single package, i.e. a class. The programmer then can then access the methods and properties of the class without having to understand the implementation.
+▶ Inheritance:   Refers to the capability of creating a new class from an existing class.
+▶ Base Class:    An originally created class, along with its attributes and behaviors. A class from which another class inherits its attributes and behaviors.
+▶ Derived Class: A newly created class, derived from a base class which inherits all of the attributes and behaviors from the base class, but may have addtional attributes and behaviors associated with it.
+
+*/
